0f3583caf77b419a23a3451a7777b6fdf6b7a15f
Dusmar6 <36971079+Dusmar6@users.noreply.github.com>
2019-11-19 13:03:26 -0800

    Update Problem 1.py

--- Problem 1.py
+++ Problem 1.py
@@ -28,9 +28,6 @@ for i in range(1, n+1):
     sigma_small_x[i-1] = std/math.sqrt(i)
     sample[i-1] = i
     
-    
-    
-print(small_x)
 #95
 conf_95_pos = np.zeros((n,1))
 conf_95_neg = np.zeros((n,1))
@@ -75,37 +72,112 @@ plt.show()
 
 
 ############## Problem 2
+M = 10000
+
 
 n = 5
 
-small_x = np.zeros((n,1))
+conf_95_succ=0
+conf_99_succ=0
 
-#sigma_small_x = np.zeros((n,1))
-#
-#sample = np.zeros((n,1))
-#
-#mu_small_x = np.full((n,1), mu)
-
-
-for i in range(1, n+1):
-    X = np.random.normal(mu, std, i)
-    small_x[i-1] = np.sum(X) / i
-#    sigma_small_x[i-1] = std/math.sqrt(i)
-#    sample[i-1] = i
-
+t_conf_95_succ=0
+t_conf_99_succ=0
+for num in range(M):
     
-small_x_mean = sum(small_x)/len(small_x)
-small_x_std = np.std(small_x)
+    small_x = np.zeros((n,1))
+    for i in range(1, n+1):
+        X = np.random.normal(mu, std, i)
+        small_x[i-1] = np.sum(X) / i
+        
+    small_x_mean = sum(small_x)/len(small_x)
+    small_x_std = np.std(small_x)
+    
+    
+    conf_95_pos = small_x_mean + (small_x_std*1.96)
+    conf_95_neg = small_x_mean - (small_x_std*1.96)
+    if (mu<conf_95_pos and mu>conf_95_neg):
+        conf_95_succ+=1
+    
+    conf_99_pos = small_x_mean + (small_x_std*2.58)
+    conf_99_neg = small_x_mean - (small_x_std*2.58)
+    if (mu<conf_99_pos and mu>conf_99_neg):
+        conf_99_succ+=1
+    
+    t_conf_95_pos = small_x_mean + (small_x_std*2.78)
+    t_conf_95_neg = small_x_mean - (small_x_std*2.78)
+    if (mu<t_conf_95_pos and mu>t_conf_95_neg):
+        t_conf_95_succ+=1
+    
+    t_conf_99_pos = small_x_mean + (small_x_std*4.6)
+    t_conf_99_neg = small_x_mean - (small_x_std*4.6)    
+    if (mu<t_conf_99_pos and mu>t_conf_99_neg):
+        t_conf_99_succ+=1
 
-conf_95_pos = small_x_mean + (small_x_std*1.96)
-conf_95_neg = small_x_mean - (small_x_std*1.96)
+conf_95_perc = conf_95_succ/M
+conf_99_perc = conf_99_succ/M
 
-t_conf_95_pos = small_x_mean + (small_x_std*2.78)
-t_conf_95_neg = small_x_mean - (small_x_std*2.78)
+t_conf_95_perc = t_conf_95_succ/M
+t_conf_99_perc = t_conf_99_succ/M
+
+print("n= ", n)
+print("conf_95_perc: ",conf_95_perc)
+print("conf_99_perc: ",conf_99_perc)
+
+print("t_conf_95_perc: ",t_conf_95_perc)
+print("t_conf_99_perc: ",t_conf_99_perc)
 
 
 
+n = 40
 
+conf_95_succ=0
+conf_99_succ=0
+
+t_conf_95_succ=0
+t_conf_99_succ=0
+for num in range(M):
+    
+    small_x = np.zeros((n,1))
+    for i in range(1, n+1):
+        X = np.random.normal(mu, std, i)
+        small_x[i-1] = np.sum(X) / i
+        
+    small_x_mean = sum(small_x)/len(small_x)
+    small_x_std = np.std(small_x)
+    
+    
+    conf_95_pos = small_x_mean + (small_x_std*1.96)
+    conf_95_neg = small_x_mean - (small_x_std*1.96)
+    if (mu<conf_95_pos and mu>conf_95_neg):
+        conf_95_succ+=1
+    
+    conf_99_pos = small_x_mean + (small_x_std*2.58)
+    conf_99_neg = small_x_mean - (small_x_std*2.58)
+    if (mu<conf_99_pos and mu>conf_99_neg):
+        conf_99_succ+=1
+    
+    t_conf_95_pos = small_x_mean + (small_x_std*2.78)
+    t_conf_95_neg = small_x_mean - (small_x_std*2.78)
+    if (mu<t_conf_95_pos and mu>t_conf_95_neg):
+        t_conf_95_succ+=1
+    
+    t_conf_99_pos = small_x_mean + (small_x_std*4.6)
+    t_conf_99_neg = small_x_mean - (small_x_std*4.6)    
+    if (mu<t_conf_99_pos and mu>t_conf_99_neg):
+        t_conf_99_succ+=1
+
+conf_95_perc = conf_95_succ/M
+conf_99_perc = conf_99_succ/M
+
+t_conf_95_perc = t_conf_95_succ/M
+t_conf_99_perc = t_conf_99_succ/M
+
+print("n= ", n)
+print("conf_95_perc: ",conf_95_perc)
+print("conf_99_perc: ",conf_99_perc)
+
+print("t_conf_95_perc: ",t_conf_95_perc)
+print("t_conf_99_perc: ",t_conf_99_perc)
 
 
 
